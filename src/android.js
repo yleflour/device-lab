0f3d80a7d2a70c6db7758043743bceb7224fa0c2
Yann Leflour <yannl@bamlab.fr>
2016-01-09 21:23:55 +0100

    Added Android apk installation

--- src/android.js
+++ src/android.js
@@ -1,40 +1,78 @@
 import adb from 'adbkit';
+import Promise from 'bluebird';
 
 var client = adb.createClient();
 var tracker;
 
-export default {
-  listDevices: function listDevices() {
-    return client.listDevices()
-    .then((devices) => {
-      for (let device of devices) {
-        console.log(device);
-      }
-    });
-  },
-  
-  startTrackingDevices: function trackDevices() {
-     client.trackDevices()
-     .then((androidTracker) => {
-       tracker = androidTracker;
-       tracker.on('add', (device) => {
-         console.log('Added Device');
-         console.log(device);
-       });
-       tracker.on('remove', (device) => {
-         console.log('Removed Device');
-         console.log(device);
-       });
-     });
-  },
-  
-  stopTrackingDevices: function stopTrackingDevices() {
-    if(tracker) {
-      tracker.end();
+function listDevices() {
+  return client.listDevices()
+  .then((devices) => {
+    for (let device of devices) {
+      console.log(device);
     }
-  },
+    return devices;
+  });
+}
   
-  installApp: function installApp() {
-    
-  },
+function startTrackingDevices() {
+    client.trackDevices()
+    .then(function (androidTracker) {
+      tracker = androidTracker;
+      tracker.on('add', function (device) {
+        console.log('Added Device');
+        console.log(device);
+      });
+      tracker.on('remove', function (device) {
+        console.log('Removed Device');
+        console.log(device);
+      });
+    });
+}
+  
+function stopTrackingDevices() {
+  if(tracker) {
+    tracker.end();
+  }
+}
+  
+function installApp(appPath) {
+  console.log(`Installing ${appPath} on all devices`);
+  
+  return client.listDevices()
+  .then(function(devices) {
+    return Promise.map(devices, function(device) {
+      console.log(`Installing on ${device.id}`);
+      return client.install(device.id, appPath)
+      .then(function() {
+        console.log(`Installed on ${device.is}`);
+      });
+    });
+  });
+}
+
+function runApp(appId) {
+  console.log(`Running ${appId} on all devices`);
+  let activity = `${appId}/.MainActivity`;
+  
+  return client.listDevices()
+  .then(function(devices) {
+    return Promise.map(devices, function(device) {
+      console.log(`Running on ${device.id}`);
+      return client.startActivity(device.id, {
+        wait: true,
+        action: activity,
+      })
+      .then(function() {
+        console.log(`Runned on ${device.is}`);
+      });
+    });
+  });
+}
+
+export default {
+  listDevices: listDevices,
+  startTrackingDevices: startTrackingDevices,
+  stopTrackingDevices: stopTrackingDevices,
+  installApp: installApp,
+  runApp: runApp,
 }
