8d55bc510acd869e920debc8d5bbee40e9cc3ce1
Yann Leflour <yannl@bamlab.fr>
2016-01-13 23:03:46 +0100

    Install app on iOS

--- src/ios.js
+++ src/ios.js
@@ -5,26 +5,47 @@ Promise.promisifyAll(iosDevice);
 
 var trackingDevices;
 
-export default {
-  listDevices: function listDevices() {
-    return iosDevice.devicesAsync()
-    .then(function (devices) {
-      for (let device of devices) {
-        console.log(device);
-      }
-    });
-  },
-  
-  startTrackingDevices: function trackDevices() {
-     trackingDevices = iosDevice.trackDevices(function (err, devices) {
-      console.log('Connected devices:');
-      console.log(devices);
-    });
-  },
-  
-  stopTrackingDevices: function stopTrackingDevices() {
-    if(trackingDevices) {
-      trackingDevices();
+function listDevices() {
+  return iosDevice.devicesAsync()
+  .then(function (devices) {
+    for (let device of devices) {
+      console.log(device);
     }
+    return devices;
+  });
+}
+  
+function startTrackingDevices() {
+    trackingDevices = iosDevice.trackDevices(function (err, devices) {
+    console.log('Connected devices:');
+    console.log(devices);
+  });
+}
+  
+function stopTrackingDevices() {
+  if(trackingDevices) {
+    trackingDevices();
   }
+}
+
+function installApp(appPath) {
+  console.log(`Installing ${appPath} on all devices`);
+  
+  return listDevices()
+  .then(function(devices) {
+    return Promise.map(devices, function(device) {
+      console.log(`Installing on ${device.name}`);
+      return iosDevice.installAppAsync(device.udid, appPath)
+      .then(function() {
+        console.log(`Installed on ${device.name}`);
+      });
+    });
+  });
+}
+
+export default {
+  listDevices: listDevices,
+  startTrackingDevices: startTrackingDevices,
+  stopTrackingDevices: stopTrackingDevices,
+  installApp: installApp,
 }
